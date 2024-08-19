d98a33a49e02f8de1130e486fab9e35afef577b9
vcosmin2701 <v.cosmin2701@gmail.com>
2024-08-19 13:58:46 +0300

    [REFACTORING] - Writing test first and then the actual implementation

--- helloWorld/hello_test.go
+++ helloWorld/hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+	got := Hello("Cosmin")
+	want := "Hello, Cosmin"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
