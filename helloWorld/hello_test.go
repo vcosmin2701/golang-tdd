3e91ca87ce8f9efcb0d65a9708a8c85c488d62be
vcosmin2701 <v.cosmin2701@gmail.com>
2024-08-19 22:25:35 +0300

    [LEARN] - How to write subtests

--- helloWorld/hello_test.go
+++ helloWorld/hello_test.go
@@ -3,10 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Cosmin")
-	want := "Hello, Cosmin"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("MyShU")
+		want := "Hello, MyShU"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
