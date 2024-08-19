d2160d1f54872d9c1a70293e7af6ac55ee6fe776
vcosmin2701 <v.cosmin2701@gmail.com>
2024-08-19 23:36:57 +0300

    [IMPLEMENTATION] - More requirements, more languages added

--- helloWorld/hello_test.go
+++ helloWorld/hello_test.go
@@ -4,12 +4,22 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("MyShU")
+		got := Hello("MyShU", "English")
 		want := "Hello, MyShU"
 		assertCorrectMessage(t, got, want)
 	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("MyShU", "Spanish")
+		want := "Hola, MyShU"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("MyShU", "French")
+		want := "Bonjour, MyShU"
+		assertCorrectMessage(t, got, want)
+	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "English")
 		want := "Hello, World"
 		assertCorrectMessage(t, got, want)
 	})
