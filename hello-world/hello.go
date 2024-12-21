9ab335917adf5b96c8fc2174581465acc023dfca
vcosmin2701 <v.cosmin2717@gmail.com>
2024-12-21 14:23:20 +0200

    Refactored with better structure

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -1,11 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func Hello(name string) string {
-	return "Hello, " + name
+const (
+	french   = "French"
+	spanish  = "Spanish"
+	romanian = "Romanian"
+
+	englishHelloPrefix  = "Hello, "
+	spanishHelloPrefix  = "Hola, "
+	frenchHelloPrefix   = "Bonjour, "
+	romanianHelloPrefix = "Salut, "
+)
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	case romanian:
+		prefix = romanianHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "French"))
 }
